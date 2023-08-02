1acf16ca7d740cd14812857ae24e2660be8f36c2
nbazis1 <126527938+nbazis1@users.noreply.github.com>
2023-08-02 18:00:01 -0500

    final push for completed Module 6 HW

---
+++ README copy.md
@@ -0,0 +1,2 @@
+# api-challenge
+Module 6 HW repo
---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "5340a229ef485d5090db2fdbb0fddf1a"
+
+# Geoapify API Key
+geoapify_key = "826c2281df934f4ead2ca06ed3e7b97b"
